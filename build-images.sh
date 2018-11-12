4db03944c730a0be50eb180025fbee78f2fd699b
Ricardo N Feliciano <FelicianoTech@gmail.com>
2018-11-11 23:25:34 -0500

    Add Docker v18.09.0.

--- build-images.sh
+++ build-images.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker build --file 18.06/Dockerfile -t cibuilds/docker:latest -t cibuilds/docker:18.06.1  -t cibuilds/docker:18.06 .
+docker build --file 18.09/Dockerfile -t cibuilds/docker:latest -t cibuilds/docker:18.09.0  -t cibuilds/docker:18.09 .
