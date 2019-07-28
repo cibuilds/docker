ca538b0619497c2e43ba062c863e3bb9b9cb4b9b
Ricardo N Feliciano <FelicianoTech@gmail.com>
2019-07-28 14:43:39 -0400

    Add Docker v19.03.1. (#20)

--- build-images.sh
+++ build-images.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker build --file 19.03/Dockerfile -t cibuilds/docker:latest -t cibuilds/docker:19.03.0  -t cibuilds/docker:19.03 .
+docker build --file 19.03/Dockerfile -t cibuilds/docker:latest -t cibuilds/docker:19.03.1  -t cibuilds/docker:19.03 .
